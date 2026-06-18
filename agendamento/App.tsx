7400a8d07cd6440e929fb1c8b9d8efbedb2f0874
Maylla Nascimento <maylla.rie@gmail.com>
2026-06-18 16:20:07 -0300

    alterando o app com o banco e a navegação

--- agendamento/App.tsx
+++ agendamento/App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { SQLiteProvider } from "expo-sqlite";
+import HomeScreen from "./src/screens/HomeScreen";
+import NovoAgendamentoScreen from "./src/screens/NovoAgendamento";
+import { inicializarBanco } from "./src/database/database";
+import DetalhesScreen from "./src/screens/DetalhesScreen";
+import { createStackNavigator } from "@react-navigation/stack";
+
+// navegar para uma nova tela ela é "empilhada" 
+// e ao voltar ela é "desempilhada"
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SQLiteProvider databaseName="agendamentos.db" onInit={inicializarBanco}>
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Detalhes" component={DetalhesScreen}/>
+            <Stack.Screen name="Novo Agendamento" component={NovoAgendamentoScreen} />
+        </Stack.Navigator>
+    </NavigationContainer>
+</SQLiteProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
