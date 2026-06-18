56633888f143a69ea86a3bc8e21fcd11c2b2a734
Maylla Nascimento <maylla.rie@gmail.com>
2026-06-18 17:24:53 -0300

    utilizando o rootstacklist para enviar o parametro

--- agendamento/App.tsx
+++ agendamento/App.tsx
@@ -4,11 +4,20 @@ import HomeScreen from "./src/screens/HomeScreen";
 import NovoAgendamentoScreen from "./src/screens/NovoAgendamento";
 import { inicializarBanco } from "./src/database/database";
 import DetalhesScreen from "./src/screens/DetalhesScreen";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// para garantir que os gestos funcionem corretamente na navegação
+import 'react-native-gesture-handler';
+import { Agendamento } from "./src/types/agendamento";
 
+export type RootStackParamList = {
+    Home: undefined;
+    NovoAgendamento: undefined;
+    Detalhes: { agendamento: Agendamento }; 
+    // vai receber o agendamento como parâmetro
+};
 // navegar para uma nova tela ela é "empilhada" 
 // e ao voltar ela é "desempilhada"
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
--- agendamento/src/screens/HomeScreen.tsx
+++ agendamento/src/screens/HomeScreen.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
-type Props = {
-  navigation: StackNavigationProp<any>;
-};
+type Props = NativeStackScreenProps<RootStackParamList>
 
 export default function HomeScreen({ navigation }: Props) {
   return (
